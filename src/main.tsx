0b636c6f4e90272be2cc2b5e1874ba22e191fe83
Zong-Rong Huang <zongrong.h886@gmail.com>
2025-12-18 18:32:37 +0800

    chore: set up msw as mock API server for dev

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,26 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import { worker } from "./mocks/browser";
+
+if (process.env.NODE_ENV === "development") {
+  // const { worker } = require("./mocks/browser");
+  console.log("to run worker");
+  await worker.start();
+}
+
+async function enableMocking() {
+  if (process.env.NODE_ENV !== "development") {
+    return;
+  }
+
+  const { worker } = await import("./mocks/browser");
+
+  // `worker.start()` returns a Promise that resolves
+  // once the Service Worker is up and ready to intercept requests.
+  return worker.start();
+}
 
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen";
@@ -17,11 +37,15 @@ declare module "@tanstack/react-router" {
 
 // Render the app
 const rootElement = document.getElementById("root")!;
-if (!rootElement.innerHTML) {
-  const root = ReactDOM.createRoot(rootElement);
-  root.render(
-    <StrictMode>
-      <RouterProvider router={router} />
-    </StrictMode>,
-  );
-}
+
+enableMocking().then(() => {
+  if (!rootElement.innerHTML) {
+    const root = ReactDOM.createRoot(rootElement);
+    root.render(
+      <StrictMode>
+        <RouterProvider router={router} />
+        <TanStackRouterDevtools initialIsOpen={false} router={router} />
+      </StrictMode>,
+    );
+  }
+});
--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -8,4 +8,20 @@ export const handlers = [
       lastName: "Maverick",
     });
   }),
+
+  http.get("https://api.example.com/query", () => {
+    return HttpResponse.json({
+      "Realtime Currency Exchange Rate": {
+        "1. From_Currency Code": "BTC",
+        "2. From_Currency Name": "Bitcoin",
+        "3. To_Currency Code": "EUR",
+        "4. To_Currency Name": "Euro",
+        "5. Exchange Rate": "75933.18000000",
+        "6. Last Refreshed": "2025-12-17 15:36:40",
+        "7. Time Zone": "UTC",
+        "8. Bid Price": "75931.67600000",
+        "9. Ask Price": "75935.81100000",
+      },
+    });
+  }),
 ];
