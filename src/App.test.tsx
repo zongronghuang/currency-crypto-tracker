791c39459b0f8ae9e690c69385529bcd47f10302
Zong-Rong Huang <zongrong.h886@gmail.com>
2025-12-22 00:35:07 +0800

    chore: remove unused files

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import { screen, render } from "@testing-library/react";
-import App from "./App";
-
-test("tttttt", () => {
-  console.log("????", import.meta.env.VITE_TITLE);
-  render(<App />);
-  const title = screen.getByRole("heading", { name: /vite/i });
-  expect(title).toBeInTheDocument();
-});
