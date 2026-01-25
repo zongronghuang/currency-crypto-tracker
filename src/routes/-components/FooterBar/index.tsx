535271edab766780265569a61574ee4436c17d5f
Zong-Rong Huang <zongrong.h886@gmail.com>
2026-01-25 13:18:33 +0800

    refactor: change z-index and portal location for FooterBar

--- src/routes/-components/FooterBar/index.tsx
+++ src/routes/-components/FooterBar/index.tsx
@@ -3,9 +3,9 @@ import { createPortal } from "react-dom";
 
 export default function FooterBar({ children }: { children: ReactNode }) {
   return createPortal(
-    <footer className="fixed bottom-0 left-0 z-5 min-h-12 w-full bg-white outline">
+    <footer className="fixed bottom-0 left-0 z-1 min-h-12 w-full bg-white outline">
       <nav>{children}</nav>
     </footer>,
-    document.querySelector("#root")!,
+    document.body,
   );
 }
