b24587f9c8ad7b25fc0b4a1fa53529e3f837874e
Zong-Rong Huang <zongrong.h886@gmail.com>
2026-01-27 18:45:19 +0800

    feat: add intersection logic to render NewsCard with a back-to-top button

--- src/routes/news/index.tsx
+++ src/routes/news/index.tsx
@@ -1,15 +1,17 @@
-import { Activity, Suspense, useState, lazy } from "react";
+import { Activity, Suspense, useState, useRef, lazy } from "react";
 import { createFileRoute } from "@tanstack/react-router";
 import { useQuery } from "@tanstack/react-query";
 import clsx from "clsx";
 import { z } from "zod";
 import dayjs from "dayjs";
 import { getNews, type GetNewsParams } from "@/apis";
+import useIntersectionObserver from "@/hooks/useIntersectionObserver";
 import NewsCard, { type Feed } from "./-components/NewsCard";
 import FooterBar from "../-components/FooterBar";
 import FooterDrawer from "../-components/FooterDrawer";
 import { FIAT_NAMES } from "@/constants/fiat-currency-list";
 import { CRYPTO_NAMES } from "@/constants/crypto-currency-list";
+import { sliceListByPage } from "@/utils";
 import styles from "./index.module.css";
 
 const NewsFilters = lazy(() => import("./-components/NewsFilters"));
@@ -21,6 +23,12 @@ const NewsSearchSchema = z.object({
     .max(3)
     .or(z.enum([...FIAT_NAMES, ...CRYPTO_NAMES]).optional()),
 });
+
+type NewsCollection = {
+  news: Feed[];
+  pageNo: number;
+  hasMore: boolean;
+};
 
 export const Route = createFileRoute("/news/")({
   component: RouteComponent,
@@ -43,31 +51,89 @@ function RouteComponent() {
 
   const [filters, setFilters] = useState({ ...defaultFilters });
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
-  const { data, isPending } = useQuery({
+  const [isBackButtonVisible, setIsBackButtonVisible] = useState(false);
+  const { data, isLoading, isSuccess } = useQuery({
     queryKey: ["news", filters],
     queryFn: () => getNews(filters),
   });
 
+  const [newsCollection, setNewsCollection] = useState<NewsCollection>({
+    news: [],
+    pageNo: 1,
+    hasMore: true,
+  });
+  const targetRef = useRef<HTMLDivElement>(null);
+  const rootRef = useRef<HTMLDivElement>(null);
+  useIntersectionObserver({
+    rootRef,
+    targetRef,
+    onIntersect: ([entry]) => {
+      if (!entry.isIntersecting || !newsCollection.hasMore || !isSuccess)
+        return;
+
+      const { pageData, hasMore } = sliceListByPage({
+        list: data.feed,
+        pageNo: newsCollection.pageNo,
+      });
+
+      setNewsCollection((prev) => ({
+        pageNo: prev.pageNo + 1,
+        news: [...prev.news, ...pageData],
+        hasMore,
+      }));
+
+      if (!hasMore) targetRef.current?.classList.remove("invisible");
+    },
+  });
+
   return (
-    <div className={clsx(styles.list)}>
-      {isPending && (
-        <p className="absolute top-1/2 left-1/2 z-10 text-2xl">Pending</p>
+    <div
+      ref={rootRef}
+      onScroll={(event) => {
+        const list = event.target as HTMLDivElement;
+        const isScrolling = list.scrollTop! > 0;
+        setIsBackButtonVisible(isScrolling);
+      }}
+      className={clsx(styles.list, "no-scrollbar h-dvh overflow-y-scroll pt-5")}
+    >
+      {isLoading ? (
+        <>
+          <NewsCard isSkeleton={true} />
+          <NewsCard isSkeleton={true} />
+          <NewsCard isSkeleton={true} />
+        </>
+      ) : (
+        newsCollection.news.map((f: Feed) => (
+          <NewsCard key={f.title} feed={f} />
+        ))
       )}
 
-      {!isPending &&
-        data.feed
-          .slice(0, 5)
-          .map((f: Feed) => <NewsCard key={f.title} feed={f} />)}
+      <div ref={targetRef} className="invisible mt-1 text-center opacity-50">
+        <small>No more results</small>
+      </div>
+
+      <button
+        aria-label="back to top"
+        hidden={!isBackButtonVisible}
+        onClick={() =>
+          rootRef.current?.scrollTo({ top: 0, behavior: "smooth" })
+        }
+        className="fixed right-2 bottom-15 z-15 h-10 w-10 -rotate-90 rounded-full bg-blue-500 text-3xl font-bold text-white shadow-[0_0_3px_2px_rgba(200,200,200,0.5)]"
+      >
+        &#10132;
+      </button>
 
       <FooterBar>
-        <div className="text-center">
+        <div className="flex h-12 items-center justify-center text-center">
           <button
+            disabled={!isSuccess}
+            className="text-4xl font-bold disabled:opacity-45"
             onClick={(event) => {
               event.stopPropagation();
               setIsDrawerOpen(true);
             }}
           >
-            Set News Filters
+            &#8943;
           </button>
         </div>
       </FooterBar>
