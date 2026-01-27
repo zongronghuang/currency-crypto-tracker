47bdf73725a8a3eda649fa410c8a87dd405527c4
Zong-Rong Huang <zongrong.h886@gmail.com>
2026-01-27 18:41:08 +0800

    feat: show skeleton UI for NewsCard for data fetching

--- src/routes/news/-components/NewsCard/index.tsx
+++ src/routes/news/-components/NewsCard/index.tsx
@@ -12,40 +12,75 @@ export type Feed = {
   topics: { topic: string }[];
 };
 
-export default function NewsCard({ feed }: { feed: Feed }) {
+const skeletonStyles =
+  "relative before:absolute before:content-[' '] before:inset-0 before:bg-gray-300 animate-pulse";
+
+const fallbackBannerImage = "src/assets/news/banner_placeholder.svg";
+
+export default function NewsCard({
+  isSkeleton = false,
+  feed,
+}: {
+  isSkeleton?: boolean;
+  feed?: Feed;
+}) {
   return (
-    <article className="overflow-clip rounded-lg shadow-md shadow-gray-400">
-      <figure className="flex w-full flex-col outline-red-500">
-        <div className="relative h-40">
+    <article className="mx-auto w-11/12 overflow-clip rounded-lg shadow-md shadow-gray-400">
+      <figure className="flex w-full flex-col">
+        <div className={clsx(isSkeleton && skeletonStyles, "relative h-40")}>
           <img
             className="block h-full w-full object-cover"
             loading="lazy"
-            src={feed.banner_image}
+            src={feed?.banner_image || fallbackBannerImage}
             alt=""
+            onError={(event) => {
+              const image = event.target as HTMLImageElement;
+              image.src = fallbackBannerImage;
+            }}
           />
           <aside className="absolute bottom-0 left-0 flex w-full flex-wrap justify-start gap-1 p-0.5">
-            {feed.topics.map((t) => (
+            {feed?.topics.map((t) => (
               <TopicTag key={t.topic} text={t.topic} />
             ))}
           </aside>
         </div>
         <figcaption className="grow p-2">
-          <div className="text-xs font-bold text-gray-500">
-            <time dateTime={feed.time_published}>
-              {dayjs(feed.time_published).format("YYYY-MM-DD")}
+          <div
+            className={clsx(
+              isSkeleton && skeletonStyles,
+              "text-xs font-bold text-gray-500",
+            )}
+          >
+            <time dateTime={feed?.time_published}>
+              {dayjs(feed?.time_published).format("YYYY-MM-DD")}
             </time>{" "}
-            | <span>{feed.source}</span>
+            | <span>{feed?.source}</span>
           </div>
 
-          <h1 className="mb-2 leading-snug font-semibold">{feed.title}</h1>
+          <h1
+            className={clsx(
+              isSkeleton && skeletonStyles,
+              "mb-2 text-lg leading-snug font-semibold",
+            )}
+          >
+            {feed?.title}
+          </h1>
 
-          <p className="relative max-h-24 overflow-hidden mask-b-from-50% text-sm leading-snug">
-            {feed.summary}
+          <p
+            className={clsx(
+              isSkeleton && skeletonStyles,
+              "relative max-h-24 min-h-12 overflow-hidden mask-b-from-50% text-sm leading-snug",
+            )}
+          >
+            {feed?.summary}
           </p>
 
           <a
-            className="ml-auto flex w-fit items-center gap-1 text-sm text-blue-500"
-            href={feed.url}
+            className={clsx(
+              isSkeleton && skeletonStyles,
+              "ml-auto flex w-fit items-center gap-1 text-sm text-blue-500",
+            )}
+            href={feed?.url}
             rel="noopener noreferrer"
             target="_blank"
           >
@@ -63,27 +98,28 @@ function getTagBgColor(tag: string) {
     case "life_sciences":
     case "energy_transportation":
     case "technology":
-      return "bg-green-600/80";
+      return "bg-green-600/90";
 
     case "financial_markets":
     case "economy_fiscal":
     case "economy_monetary":
     case "economy_macro":
     case "finance":
-      return "bg-blue-600/80";
+      return "bg-blue-600/90";
 
     case "mergers_and_acquisitions":
     case "manufacturing":
     case "retail_wholesale":
     case "real_estate":
-      return "bg-red-600/80";
+      return "bg-stone-600/90";
 
     case "earnings":
     case "ipo":
     case "blockchain":
-      return "bg-[#b8860b]/80";
+      return "bg-yellow-600/90";
+
     default:
-      return "text-neutral-600/80";
+      return "text-neutral-600/90";
   }
 }
 
