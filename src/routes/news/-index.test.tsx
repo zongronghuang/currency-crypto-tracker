d22de503595e49ad495d3f07a3d282bfff646c0f
Zong-Rong Huang <zongrong.h886@gmail.com>
2026-03-02 15:38:52 +0800

    test: add tests for showing alerts when there's server error and when there's no matching news

--- src/routes/news/-index.test.tsx
+++ src/routes/news/-index.test.tsx
@@ -1,4 +1,6 @@
 import { screen, within, waitFor } from "@testing-library/react";
+import { http, HttpResponse } from "msw";
+import { server } from "@/mocks/server";
 import { renderWithFileRoutes } from "@/mocks/file-route-utils";
 
 test("while loading news data, the app has three skeleton cards and the footer button is disabled", async () => {
@@ -31,4 +33,45 @@ test("when data is fully loaded and rendered into cards, no skeleton cards are p
   const footer = screen.getByRole("contentinfo");
   const footerButton = within(footer).getByRole("button");
   waitFor(() => expect(footerButton).toBeEnabled());
+});
+
+test("shows no-news alert when no news data is available based on current filters", async () => {
+  server.resetHandlers(
+    http.get("https://api.example.com/news", async () => {
+      return HttpResponse.json({
+        items: "",
+        sentiment_score_definition: "",
+        relevance_score_definition: "",
+        feed: [],
+      });
+    }),
+  );
+
+  renderWithFileRoutes(<div />, {
+    initialLocation: "/news",
+  });
+
+  const newsCards = screen.queryAllByRole("article");
+  expect(newsCards).toHaveLength(0);
+
+  const noDataAlert = await screen.findByRole("alert");
+  expect(noDataAlert).toBeVisible();
+});
+
+test.only("shows alert when server error occurs", async () => {
+  server.resetHandlers(
+    http.get("https://api.example.com/news", async () => {
+      return new HttpResponse(null, { status: 500 });
+    }),
+  );
+
+  renderWithFileRoutes(<div />, {
+    initialLocation: "/news",
+  });
+
+  const newsCards = screen.queryAllByRole("article");
+  expect(newsCards).toHaveLength(0);
+
+  const noDataAlert = await screen.findByRole("alert");
+  expect(noDataAlert).toBeVisible();
 });
