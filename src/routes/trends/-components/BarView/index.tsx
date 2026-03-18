1cccdcc0b14d07964d10cdf1be0b38ba60338a75
Zong-Rong Huang <zongrong.h886@gmail.com>
2026-03-18 16:40:43 +0800

    feat: add bar chart for currency exchange data

---
+++ src/routes/trends/-components/BarView/index.tsx
@@ -0,0 +1,27 @@
+import { Chart, BarSeries } from "lightweight-charts-react-components";
+import { type DeepPartial, type TimeChartOptions } from "lightweight-charts";
+import { type FiatItem, type CryptoItem } from "../../-types";
+
+export default function BarView({
+  series,
+  chartOptions,
+}: {
+  series: (FiatItem | CryptoItem)[];
+  chartOptions: DeepPartial<TimeChartOptions>;
+}) {
+  const barData = series.map((s) => ({
+    time: s.time,
+    open: +s.open,
+    high: +s.high,
+    low: +s.low,
+    close: +s.close,
+  }));
+
+  return (
+    <div>
+      <Chart options={chartOptions}>
+        <BarSeries data={barData} />
+      </Chart>
+    </div>
+  );
+}
