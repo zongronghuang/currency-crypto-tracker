4d009010489c529d110d148f9f3a1e59223d0ad7
Zong-Rong Huang <zongrong.h886@gmail.com>
2026-03-22 13:33:04 +0800

    feat: add candlestick chart for currency exchange data

---
+++ src/routes/trends/-components/CandlestickView/index.tsx
@@ -0,0 +1,31 @@
+import { Chart, CandlestickSeries } from "lightweight-charts-react-components";
+import { type DeepPartial, type TimeChartOptions } from "lightweight-charts";
+import { type FiatItem, type CryptoItem } from "../../-types";
+
+export default function CandlestickView({
+  series,
+  chartOptions,
+}: {
+  series: (FiatItem | CryptoItem)[];
+  chartOptions: DeepPartial<TimeChartOptions>;
+}) {
+  const candlestickData = series.map((s) => ({
+    time: s.time,
+    open: +s.open,
+    high: +s.high,
+    low: +s.low,
+    close: +s.close,
+  }));
+
+  return (
+    <div>
+      <p className="mb-2 text-sm text-gray-600">
+        Check the open, close, high, and low prices of the base currency in the
+        quote currency.
+      </p>
+      <Chart options={chartOptions}>
+        <CandlestickSeries data={candlestickData} />
+      </Chart>
+    </div>
+  );
+}
