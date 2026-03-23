64202b018ce3d4e8f0189ce3f025e5fce9452d79
Zong-Rong Huang <zongrong.h886@gmail.com>
2026-03-23 13:35:17 +0800

    feat: create /trends page with components and data

--- src/routes/trends/index.tsx
+++ src/routes/trends/index.tsx
@@ -1,9 +1,322 @@
+import {
+  Activity,
+  Suspense,
+  useState,
+  lazy,
+  type Dispatch,
+  type SetStateAction,
+} from "react";
 import { createFileRoute } from "@tanstack/react-router";
+import { useQuery } from "@tanstack/react-query";
+import clsx from "clsx";
+import { type DeepPartial, type TimeChartOptions } from "lightweight-charts";
+import { type TrendsParams, getTrends } from "@/apis";
+import FooterBar from "../-components/FooterBar";
+import FooterDrawer from "../-components/FooterDrawer";
+import Alert from "../-components/Alert";
+import { CRYPTO_TRADING_PAIRS } from "@/constants/crypto-exchange-list";
+import {
+  type Trends,
+  type FiatPrices,
+  type CryptoPrices,
+  type CryptoItem,
+  type FiatItem,
+} from "./-types";
+import styles from "./index.module.css";
+
+const TrendsMenu = lazy(() => import("./-components/TrendsMenu"));
+const TableView = lazy(() => import("./-components/TableView"));
+const LineView = lazy(() => import("./-components/LineView"));
+const BarView = lazy(() => import("./-components/BarView"));
+const CandlestickView = lazy(() => import("./-components/CandlestickView"));
+const HistogramView = lazy(() => import("./-components/HistogramView"));
+
+const views = ["table", "line", "bar", "candlestick", "histogram"] as const;
+
+const chartOptions: DeepPartial<TimeChartOptions> = {
+  height: 450,
+  autoSize: true,
+  timeScale: { fixLeftEdge: true, fixRightEdge: true },
+  layout: {
+    panes: { enableResize: true },
+  },
+};
+
+const defaultTrendsApiParams: TrendsParams = {
+  dataPoint: "daily",
+  base: "USD",
+  quote: "EUR",
+};
+
+type View = (typeof views)[number];
+type Entry = [string, FiatPrices | CryptoPrices];
 
 export const Route = createFileRoute("/trends/")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <div>Hello /trends/!</div>;
+  const [trendsApiParams, setTrendsApiParams] = useState<TrendsParams>({
+    ...defaultTrendsApiParams,
+  });
+  const { data, isSuccess, isPending, isError, error } = useQuery({
+    queryKey: ["trends", trendsApiParams],
+    queryFn: () => getTrends(trendsApiParams),
+  });
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const [view, setView] = useState<View>("table");
+  const baseCategory =
+    trendsApiParams.base in CRYPTO_TRADING_PAIRS ? "crypto" : "fiat";
+
+  if (isError) return <Alert title="Oops" description={error.message} />;
+
+  const { metaData, timeSeriesArray, startDate, endDate } =
+    extractTrendsData(data);
+
+  return (
+    <div className={styles.trends}>
+      {isPending && (
+        <Alert title="Loading" description="Data is arriving now." />
+      )}
+
+      {isSuccess && (
+        <>
+          <BulletinBoard
+            metaData={metaData}
+            startDate={startDate}
+            endDate={endDate}
+          />
+
+          <section className="mx-auto w-full">
+            <ViewOptions
+              view={view}
+              setView={setView}
+              baseCategory={baseCategory}
+            />
+            <Suspense
+              fallback={<Alert title="Rendering" description="Ready soon!" />}
+            >
+              <DataView
+                view={view}
+                series={timeSeriesArray}
+                chartOptions={chartOptions}
+              />
+            </Suspense>
+          </section>
+        </>
+      )}
+
+      <FooterBar>
+        <div className="flex h-12 items-center justify-center text-center">
+          <button
+            aria-label="open drawer button"
+            aria-expanded={isDrawerOpen}
+            disabled={!isSuccess}
+            className="text-4xl font-bold disabled:opacity-45"
+            onClick={(event) => {
+              event.stopPropagation();
+              setIsDrawerOpen(true);
+            }}
+          >
+            &#8943;
+          </button>
+        </div>
+      </FooterBar>
+
+      <FooterDrawer open={isDrawerOpen} onClose={() => setIsDrawerOpen(false)}>
+        <Suspense>
+          <Activity mode="visible">
+            <TrendsMenu
+              trendsApiParams={trendsApiParams}
+              setTrendsApiParams={setTrendsApiParams}
+              onClose={() => setIsDrawerOpen(false)}
+            />
+          </Activity>
+        </Suspense>
+      </FooterDrawer>
+    </div>
+  );
+}
+
+function BulletinBoard({
+  metaData,
+  startDate,
+  endDate,
+}: {
+  metaData: { [key: string]: string };
+  startDate: string;
+  endDate: string;
+}) {
+  const title = metaData["1. Information"];
+  const baseCurrency =
+    metaData["2. From Symbol"] || metaData["2. Digital Currency Code"];
+  const quoteCurrency = metaData["3. To Symbol"] || metaData["4. Market Code"];
+  const lastRefreshed =
+    metaData["5. Last Refreshed"] || metaData["6. Last Refreshed"];
+  const timeZone = metaData["6. Time Zone"] || metaData["7. Time Zone"];
+
+  return (
+    <section
+      aria-label="bulletin board"
+      className="mb-2 rounded-lg border-4 border-double border-emerald-50 bg-emerald-200 p-2 text-sm"
+    >
+      <h1 className="font-semibold">{title}</h1>
+      <h2>
+        Base/quote:{" "}
+        <span className="font-semibold">
+          {baseCurrency} &#8260; {quoteCurrency}{" "}
+        </span>
+      </h2>
+      <p>
+        Time:{" "}
+        <time dateTime={startDate} className="font-semibold">
+          {startDate}
+        </time>{" "}
+        &#8213;{" "}
+        <time dateTime={endDate} className="font-semibold">
+          {endDate}
+        </time>
+      </p>
+      <p>
+        Last refreshed:{" "}
+        <time dateTime={lastRefreshed} className="font-semibold">
+          {lastRefreshed} ({timeZone})
+        </time>
+      </p>
+    </section>
+  );
+}
+
+function ViewOptions({
+  view,
+  baseCategory,
+  setView,
+}: {
+  view: View;
+  baseCategory: "crypto" | "fiat";
+  setView: Dispatch<SetStateAction<View>>;
+}) {
+  const [isListVisible, setIsListVisible] = useState(false);
+  const remainingViews = views.filter((v) => {
+    const isActiveView = v === view;
+    return baseCategory === "fiat"
+      ? !isActiveView && v !== "histogram"
+      : !isActiveView;
+  });
+
+  const capitalize = (text: string) => text[0].toUpperCase() + text.slice(1);
+
+  return (
+    <div className="relative mb-2 ml-auto flex w-fit flex-row-reverse gap-3 leading-none">
+      <button
+        title={capitalize(view)}
+        className={clsx(
+          isListVisible && "border-blue-600",
+          "w-8 rounded-sm border border-solid p-0.5 shadow-md",
+        )}
+        onClick={() => setIsListVisible((prev) => !prev)}
+      >
+        <img src={`/src/assets/trends/${view}.svg`} alt={view} />
+      </button>
+
+      {remainingViews.map((v) => (
+        <button
+          key={v}
+          title={capitalize(v)}
+          className={clsx(
+            isListVisible
+              ? "pointer-events-auto opacity-100"
+              : "pointer-events-none opacity-0",
+            "w-8 rounded-sm border border-solid p-0.5 transition-all hover:border-blue-600",
+          )}
+          onClick={() => {
+            setIsListVisible(false);
+            setView(v);
+          }}
+        >
+          <img src={`/src/assets/trends/${v}.svg`} alt={v} />
+        </button>
+      ))}
+    </div>
+  );
+}
+
+function DataView({
+  view,
+  series,
+  chartOptions,
+}: {
+  view: View;
+  series: (FiatItem | CryptoItem)[];
+  chartOptions: DeepPartial<TimeChartOptions>;
+}) {
+  switch (view) {
+    case "table":
+      return <TableView series={series} />;
+    case "line":
+      return <LineView series={series} chartOptions={chartOptions} />;
+    case "bar":
+      return <BarView series={series} chartOptions={chartOptions} />;
+    case "candlestick":
+      return <CandlestickView series={series} chartOptions={chartOptions} />;
+    case "histogram":
+      return (
+        <HistogramView
+          series={series as CryptoItem[]}
+          chartOptions={chartOptions}
+        />
+      );
+    default:
+      return (
+        <Alert title="No Matching View" description="Check your view option." />
+      );
+  }
+}
+
+function extractTrendsData(trendsData: Trends) {
+  if (!trendsData)
+    return {
+      metaData: {},
+      timeSeriesArray: [],
+      startDate: "",
+      endDate: "",
+    };
+
+  const entries = Object.entries(trendsData);
+
+  const metaData = entries.find(([key]) => key === "Meta Data")![1];
+
+  const timeSeries = entries.find(([key]) => key.includes("Time Series"))![1];
+
+  const dataEntries = Object.entries(timeSeries) as Entry[];
+  const timeSeriesArray = dataEntries
+    .sort((a, b) => a[0].localeCompare(b[0]))
+    .map(([time, data], i, arr) => {
+      const prevData: Entry = arr[i - 1];
+      const formattedData: { [key: string]: string } = {
+        time,
+        high: data["2. high"]!,
+        low: data["3. low"],
+        open: data["1. open"],
+        close: data["4. close"],
+        prevClose: prevData ? prevData[1]["4. close"] : "",
+      };
+
+      const hasVolume = "5. volume" in data;
+      if (hasVolume) {
+        formattedData.volume = data["5. volume"];
+      }
+
+      return formattedData as FiatItem | CryptoItem;
+    });
+
+  const endDate = timeSeriesArray.at(-1)!.time;
+  const startDate = timeSeriesArray[0].time;
+
+  return {
+    metaData,
+    timeSeriesArray,
+    startDate,
+    endDate,
+  };
 }
