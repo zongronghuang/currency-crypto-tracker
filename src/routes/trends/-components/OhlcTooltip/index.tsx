065135c2cf90061f266501e5663d896950352a50
Zong-Rong Huang <zongrong.h886@gmail.com>
2026-04-12 13:12:15 +0800

    refactor: create OhlcTooltip for reuse

---
+++ src/routes/trends/-components/OhlcTooltip/index.tsx
@@ -0,0 +1,55 @@
+import { type RefObject } from "react";
+import { type Time } from "lightweight-charts";
+import clsx from "clsx";
+
+export default function OhlcTooltip({
+  ref,
+  isVisible,
+  time,
+  high,
+  low,
+  open,
+  close,
+}: {
+  ref: RefObject<HTMLDivElement | null>;
+  isVisible: boolean;
+  time: Time;
+  high?: number;
+  low?: number;
+  open?: number;
+  close?: number;
+}) {
+  return (
+    <div
+      ref={ref}
+      className={clsx(
+        isVisible ? "visible" : "invisible",
+        "absolute z-5 w-fit rounded-lg bg-blue-600 p-2 text-xs font-light text-white shadow-sm",
+      )}
+    >
+      <h3>{time.toString()}</h3>
+      <dl className="grid grid-cols-2">
+        {open && (
+          <>
+            <dt>Open</dt> <dd>{open}</dd>
+          </>
+        )}
+        {high && (
+          <>
+            <dt>High</dt> <dd>{high}</dd>
+          </>
+        )}
+        {low && (
+          <>
+            <dt>Low</dt> <dd>{low}</dd>
+          </>
+        )}
+        {close && (
+          <>
+            <dt>Close</dt> <dd>{close}</dd>
+          </>
+        )}
+      </dl>
+    </div>
+  );
+}
