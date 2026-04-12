c6c74563e1660e5e72ff272b587e35ae545f4eb4
Zong-Rong Huang <zongrong.h886@gmail.com>
2026-04-12 12:47:47 +0800

    feat: add tooltip to CandlestickView to display data by temporal point

--- src/routes/trends/-components/CandlestickView/index.tsx
+++ src/routes/trends/-components/CandlestickView/index.tsx
@@ -1,10 +1,18 @@
-import { useState } from "react";
+import { useState, useRef, type RefObject } from "react";
 import {
   Chart,
   CandlestickSeries,
   PriceLine,
+  type SeriesApiRef,
 } from "lightweight-charts-react-components";
-import { type DeepPartial, type TimeChartOptions } from "lightweight-charts";
+import {
+  type DeepPartial,
+  type Time,
+  type TimeChartOptions,
+  type OhlcData,
+  type Point,
+} from "lightweight-charts";
+import clsx from "clsx";
 import { getRates } from "../../-helpers";
 import { type FiatItem, type CryptoItem } from "../../-types";
 
@@ -56,6 +64,17 @@ export default function CandlestickView({
   series: (FiatItem | CryptoItem)[];
   chartOptions: DeepPartial<TimeChartOptions>;
 }) {
+  const seriesRef = useRef<SeriesApiRef<"Candlestick">>(null);
+  const tooltipRef = useRef<HTMLDivElement>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [tooltipData, setTooltipData] = useState<OhlcData<Time>>({
+    time: "2000-01-01",
+    open: 0,
+    high: 0,
+    low: 0,
+    close: 0,
+  });
+  const [isTooltipVisible, setIsToolTipVisible] = useState(false);
   const [visibleRateLines, setVisibleRateLines] = useState<VisibleRateLines>({
     min: false,
     avg: false,
@@ -124,17 +143,54 @@ export default function CandlestickView({
         </label>
       </div>
 
-      <Chart options={chartOptions}>
-        <CandlestickSeries data={candlestickData}>
-          {enabledRateLines.map(({ price, options }) => (
-            <PriceLine
-              key={`${options?.title}-${price}`}
-              options={options}
-              price={+price}
-            />
-          ))}
-        </CandlestickSeries>
-      </Chart>
+      <div className="relative">
+        <Chart
+          ref={containerRef}
+          options={chartOptions}
+          onCrosshairMove={(params) => {
+            console.log({ point: params.point });
+            if (!seriesRef.current || !params.point) return;
+
+            const seriesApi = seriesRef.current.api();
+            if (!seriesApi) return;
+
+            const data = params.seriesData.get(seriesApi) as OhlcData<Time>;
+            if (!data) return;
+
+            setIsToolTipVisible(true);
+            setTooltipData(data);
+
+            requestAnimationFrame(() => {
+              const { x, y } = getTooltipPosition(
+                containerRef,
+                tooltipRef,
+                params.point!,
+              );
+              tooltipRef.current!.style.left = `${x}px`;
+              tooltipRef.current!.style.top = `${y}px`;
+            });
+          }}
+        >
+          <CandlestickSeries data={candlestickData} ref={seriesRef}>
+            {enabledRateLines.map(({ price, options }) => (
+              <PriceLine
+                key={`${options?.title}-${price}`}
+                options={options}
+                price={+price}
+              />
+            ))}
+          </CandlestickSeries>
+          <Tooltip
+            isVisible={isTooltipVisible}
+            ref={tooltipRef}
+            time={tooltipData.time}
+            high={tooltipData.high}
+            low={tooltipData.low}
+            open={tooltipData.open}
+            close={tooltipData.close}
+          />
+        </Chart>
+      </div>
     </div>
   );
 }
@@ -146,4 +202,83 @@ function generateRateLines(rates: Rates, visibleRateLines: VisibleRateLines) {
   if (visibleRateLines.avg) lines.push({ ...avgRateLine, price: rates.avg });
 
   return lines;
+}
+
+function Tooltip({
+  ref,
+  isVisible,
+  time,
+  high,
+  low,
+  open,
+  close,
+}: {
+  ref: RefObject<HTMLDivElement | null>;
+  isVisible: boolean;
+  time: Time;
+  high?: number;
+  low?: number;
+  open?: number;
+  close?: number;
+}) {
+  return (
+    <div
+      ref={ref}
+      className={clsx(
+        isVisible ? "visible" : "invisible",
+        "absolute z-5 w-fit rounded-lg bg-blue-600 p-2 text-xs font-light text-white shadow-sm",
+      )}
+    >
+      <h3>{time.toString()}</h3>
+      <dl className="grid grid-cols-2">
+        {open && (
+          <>
+            <dt>Open</dt> <dd>{open}</dd>
+          </>
+        )}
+        {high && (
+          <>
+            <dt>High</dt> <dd>{high}</dd>
+          </>
+        )}
+        {low && (
+          <>
+            <dt>Low</dt> <dd>{low}</dd>
+          </>
+        )}
+        {close && (
+          <>
+            <dt>Close</dt> <dd>{close}</dd>
+          </>
+        )}
+      </dl>
+    </div>
+  );
+}
+
+function getTooltipPosition(
+  containerRef: RefObject<HTMLDivElement | null>,
+  tooltipRef: RefObject<HTMLDivElement | null>,
+  crosshairPosition: Point,
+) {
+  const containerWidth = containerRef.current!.clientWidth;
+  const tooltipWidth = tooltipRef.current!.clientWidth;
+  const tooltipHeight = tooltipRef.current!.clientHeight;
+
+  const defaultX = crosshairPosition.x - tooltipWidth / 2;
+  const defaultY = crosshairPosition.y - tooltipHeight * 1.2;
+
+  let x = defaultX;
+  let y = defaultY;
+
+  const isNearLeftEdge = defaultX <= 0;
+  const isNearRightEdge = defaultX + tooltipWidth >= containerWidth;
+  if (isNearLeftEdge) x = defaultX + tooltipWidth / 2;
+  if (isNearRightEdge) x = defaultX - tooltipWidth / 2;
+
+  const isNearTopEdge = defaultY <= 0;
+  const minDistanceY = 30;
+  if (isNearTopEdge) y = crosshairPosition.y + minDistanceY;
+
+  return { x, y };
 }
