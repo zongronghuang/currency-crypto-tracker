25bdf9f99d6f665a0401e75da31a6a838093d428
Zong-Rong Huang <zongrong.h886@gmail.com>
2026-05-02 14:23:01 +0800

    feat: add responsive styles to HeaderBar

--- src/components/HeaderBar/index.tsx
+++ src/components/HeaderBar/index.tsx
@@ -14,15 +14,15 @@ export default function HeaderBar({ setIsSideMenuOpen }: HeaderBarProps) {
   return (
     <header
       role="banner"
-      className="sticky top-0 left-0 z-10 flex w-full items-center justify-between bg-white px-5 py-3 shadow-md md:px-8 md:py-4 xl:fixed xl:right-0 xl:ml-auto xl:py-2 xl:shadow-none"
+      className="sticky top-0 left-0 z-10 flex w-full items-center justify-between bg-white px-5 py-2 shadow-md md:px-8 xl:fixed xl:right-0 xl:ml-auto xl:shadow-none"
     >
       <button
         type="button"
         aria-label="open side menu"
-        className="rounded bg-blue-600 p-1 text-center text-white hover:bg-blue-700 active:bg-blue-800 md:rounded-lg md:p-1.5 xl:w-0 xl:p-0"
+        className="rounded bg-blue-600 p-1 text-center text-white hover:bg-blue-700 active:bg-blue-800 md:rounded-lg md:p-1.5 xl:pointer-events-none xl:invisible"
         onClick={() => setIsSideMenuOpen(true)}
       >
-        <SideMenuIcon className="h-auto w-6 md:w-12 xl:hidden" />
+        <SideMenuIcon className="h-auto w-6 md:w-8" />
       </button>
 
       <h1 className="text-xl font-bold text-slate-900 uppercase md:text-3xl lg:text-4xl xl:text-2xl">
@@ -31,9 +31,9 @@ export default function HeaderBar({ setIsSideMenuOpen }: HeaderBarProps) {
 
       <button
         aria-label="open user profile"
-        className="rounded bg-blue-600 p-1 text-center text-white hover:bg-blue-700 active:bg-blue-800 md:rounded-lg"
+        className="rounded bg-blue-600 p-1 text-center text-white hover:bg-blue-700 active:bg-blue-800 md:rounded-lg md:p-1.5"
       >
-        <UserIcon className="h-auto w-6 md:w-12 xl:w-8" />
+        <UserIcon className="h-auto w-6 md:w-8" />
       </button>
     </header>
   );
