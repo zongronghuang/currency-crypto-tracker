a3638ff1f0a6ea6765676fabc6a9cf55760f87ce
Zong-Rong Huang <zongrong.h886@gmail.com>
2026-05-02 14:25:46 +0800

    feat: add responsive paddings and margins to page container

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -13,7 +13,7 @@ function RootComponent() {
   return (
     <>
       <HeaderBar setIsSideMenuOpen={setIsSideMenuOpen} />
-      <main className="xl:pr-2xl px-5 pt-5 pb-15 md:mx-auto md:w-11/12 md:p-0 md:pt-16 xl:mt-[10vh] xl:ml-[13vw] xl:w-[85vw] xl:pt-0">
+      <main className="xl:pr-2xl px-5 pt-5 pb-15 md:mx-auto md:w-10/12 md:p-0 xl:mt-[8vh] xl:ml-[13vw] xl:w-[85vw] xl:pt-0">
         <Outlet />
       </main>
       <SideMenu
